92cce1aa8cc212d1ef595a40ec5c9ae29a515440
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2020-03-17 19:37:02 +0200

    Implement episodes with unidirectional point sources
    The 3D case needs some work, but at least it's smooth.

--- episodes.py
+++ episodes.py
@@ -91,7 +91,78 @@ def animated_room():
     FuncAnimation(pylab.gcf(), update, frames=range(100), init_func=lambda: plots, blit=True, repeat=True, interval=10)
     pylab.show()
 
+
+def animated_point_source_in_room():
+    from scipy.special import dawsn
+    N = 128
+    x = np.linspace(-2, 2, N+1)[:-1] + 0j
+    dx = np.real(x[1]-x[0])
+    M = N//2
+    y = z = x
+    x, y, z = np.meshgrid(x, y, z, indexing='ij')
+
+    # Inwards surging spherical wave with a gaussian profile
+    # TODO: Figure out how to do an actual point source
+    r = np.sqrt(x*x+y*y+z*z)
+    r1 = 20*r-15
+    u = -(np.exp(-r1**2) + 2j/np.sqrt(np.pi)*dawsn(r1)) / (r+1)
+
+    plots = pylab.plot(np.real(u[M,M]))
+    plots.extend(pylab.plot(np.imag(u[M,M])))
+    pylab.show()
+
+    boundary = 0*x
+
+    wave_equation = WaveEquation(u, boundary, dx, dt=dx, decay=0)
+
+    plots = [pylab.imshow(wave_equation.numpy()[:,:,M], vmin=-0.05, vmax=0.05)]
+
+    def update(frame):
+        wave_equation.step()
+        print(wave_equation.t)
+        plots[0].set_data(wave_equation.numpy()[:,:,M])
+        return plots
+
+    FuncAnimation(pylab.gcf(), update, frames=range(18), init_func=lambda: plots, blit=True, repeat=True, interval=20)
+    pylab.show()
+
+
+def animated_point_source_on_string():
+    x = np.linspace(-1.2, 1.2, 128) + 0j
+    dx = np.real(x[1]-x[0])
+
+    u = 0*x
+    u[64] = 1
+
+    v = np.fft.fftn(u)
+    v[65:] *= 0
+    v[64] *= 0.5
+    u = np.fft.ifftn(v)
+
+    pylab.plot(np.real(u))
+    pylab.plot(np.imag(u))
+    pylab.show()
+
+    boundary = 0*x
+
+    wave_equation = WaveEquation(u, boundary, dx, dt=dx, decay=0)
+
+    plots = pylab.plot(wave_equation.numpy())
+
+    def update(frame):
+        for _ in range(1):
+            wave_equation.step()
+        print(wave_equation.t)
+        plots[0].set_ydata(wave_equation.numpy())
+        return plots
+
+    FuncAnimation(pylab.gcf(), update, frames=range(100), init_func=lambda: plots, blit=True, repeat=True, interval=10)
+    pylab.show()
+
+
 if __name__ == '__main__':
     # animated_string()
     # animated_membrane()
-    animated_room()
+    # animated_room()
+    animated_point_source_in_room()
+    # animated_point_source_on_string()
