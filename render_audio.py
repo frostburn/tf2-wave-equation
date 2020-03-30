216e8f9801308af67eb657c654d834f9d6c038d9
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2020-03-30 12:25:34 +0300

    Render audio for a tetrahedron struck on the side

--- render_audio.py
+++ render_audio.py
@@ -3,6 +3,7 @@ from __future__ import division
 import argparse
 import numpy as np
 import pylab
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import progressbar
 
 import shapes
@@ -25,7 +26,17 @@ def triangle(N):
     return x, y, dx, (triangle_ > 0.52)
 
 
-def process_triangle_drum(args, integrator_params):
+def tetrahedron(N):
+    x = np.arange(-1, 1, 2/N)
+    dx = x[1] - x[0]
+    x, y, z = np.meshgrid(x, x, x, indexing='ij')
+
+    tetrahedron_ = shapes.regular_tetrahedron(x, y, z)
+
+    return x, y, z, dx, (tetrahedron_ > 0.4)
+
+
+def strike_triangle_drum(args, integrator_params):
     N = args.grid_width
     x, y, dx, boundary = triangle(N)
 
@@ -77,11 +88,42 @@ def triangle_resonances(args, integrator_params):
     write_audio(args.outfile, data, sample_rate=args.sample_rate)
 
 
+def strike_tetrahedron(args, integrator_params):
+    N = args.grid_width
+    x, y, z, dx, boundary = tetrahedron(N)
+
+    ex_x, ex_y, ex_z = np.random.randn(3) * args.variation * 0.2 + np.array([0.55, 0.1, 0.13])
+    exitation = np.exp(-args.sharpness**2*((x-ex_x)**2 + (y-ex_y)**2 + (z-ex_z)**2))
+    exitation *= 1 - boundary
+
+    integrator = WaveEquation(exitation, boundary, dx, **integrator_params)
+
+    left_channel = []
+    right_channel = []
+
+    for i in progressbar.progressbar(range(int(args.sample_rate * args.duration))):
+        left_channel.append(np.real(integrator.u[int(N*0.4), int(N*0.3), int(N*0.4)].numpy()))
+        right_channel.append(np.real(integrator.u[int(N*0.4), int(N*0.3), int(N*0.36)].numpy()))
+        integrator.step()
+
+    data = np.array([left_channel, right_channel])
+    data /= abs(data).max()
+
+    write_audio(args.outfile, data, sample_rate=args.sample_rate)
+
+
 if __name__ == '__main__':
+    episodes = {
+        'drum': strike_triangle_drum,
+        'resonance': triangle_resonances,
+        'tetrahedron': strike_tetrahedron,
+    }
+
+
     parser = argparse.ArgumentParser(description='Render audio samples')
-    parser.add_argument('episode', choices=['drum', 'resonance'])
+    parser.add_argument('episode', choices=episodes.keys())
     parser.add_argument('outfile', type=str, help='Output file name')
-    parser.add_argument('--grid-width', type=int, help='Number of grid points along a grid edge', default=128)
+    parser.add_argument('--grid-width', type=int, help='Number of grid points along a grid edge', metavar='N', default=128)
     parser.add_argument('--sample-rate', type=int, help='Sampling rate of the resulting audio file', default=48000)
     parser.add_argument('--duration', type=float, help='Duration of audio to render', default=1.0)
     parser.add_argument('--sharpness', type=float, help='Sharpness of the initial hit', default=10.0)
@@ -97,7 +139,4 @@ if __name__ == '__main__':
         'dispersion': args.dispersion,
     }
 
-    if args.episode == 'drum':
-        process_triangle_drum(args, integrator_params)
-    elif args.episode == 'resonance':
-        triangle_resonances(args, integrator_params)
+    episodes[args.episode](args, integrator_params)
